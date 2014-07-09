45770d2c64e0d6fd11f1ce935147c0dbc57cc353
Brian Claywell <bclaywel@fhcrc.org>
2014-07-08 21:09:56 -0700

    Run Galaxy as an unprivileged user.

--- base/startup.sh
+++ base/startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+set -e
 service nginx start
 
 cd /galaxy/stable
-sh run.sh
+test -z "$VIRTUAL_ENV" && source /galaxy/galaxy-env/bin/activate
+# gosu doesn't set HOME, so set this manually
+export PYTHON_EGG_CACHE="/galaxy/.python-eggs"
+exec gosu galaxy sh run.sh
