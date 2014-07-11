7c5d2f3f56800dcbd897c8fdce64ca2adcd80237
Brian Claywell <bclaywel@fhcrc.org>
2014-07-10 20:55:56 -0700

    Add optional postgres link support.

--- base/startup.sh
+++ base/startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
+
 service nginx start
 
 cd /galaxy/stable
 test -z "$VIRTUAL_ENV" && source /galaxy/galaxy-env/bin/activate
-# gosu doesn't set HOME, so set this manually
-export PYTHON_EGG_CACHE="/galaxy/.python-eggs"
+
+if [ -n "${DB_NAME}" ]; then
+    DB_CONN="postgresql://${DB_USER:=galaxy}:${DB_PASS:=galaxy}@${DB_PORT_5432_TCP_ADDR}:${DB_PORT_5432_TCP_PORT}/${DB_DATABASE:=galaxy}"
+    sed -i 's|^database_connection = .*$|database_connection = '${DB_CONN}'|' universe_wsgi.ini
+fi
+
 exec gosu galaxy sh run.sh
