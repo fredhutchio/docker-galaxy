23fb84924ee01cd1cb5eb5e4b39615428567eaf1
Brian Claywell <bclaywel@fhcrc.org>
2014-08-29 12:31:33 -0700

    Make docker-link-exports a little safer.

--- docker-link-exports.sh
+++ docker-link-exports.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-set +eu
+set -eu
 
 # This script expects that DATA_EXPORT_DIR and DATA_EXPORTS variables
 # are set, either in the Dockerfile or at runtime. If an exported
 # directory doesn't yet exist in EXPORT_DIR, move the container's data
 # over before symlinking the export into place.
 if [ -d ${DATA_EXPORT_DIR} ]; then
-    for dir in ${DATA_EXPORTS}; do
-        # If the directory doesn't exist in /export, copy it over.
-        if [ ! -d ${DATA_EXPORT_DIR}/${dir} ]; then
-            echo -n "Migrating ${dir} to ${DATA_EXPORT_DIR}$dir... "
-            # Since most of this will be many small text files, compress
-            # and stream the data (instead of mv) in case /export is
-            # actually mounted over a network.
-            tar cpz -C / ${dir} 2> /dev/null | tar xpzf - -C ${DATA_EXPORT_DIR}
+    for source in ${DATA_EXPORTS}; do
+        target="${DATA_EXPORT_DIR}${source}"
+        target_dir=$(dirname target)
+
+        # If the export doesn't exist in DATA_EXPORT_DIR, copy ours over.
+        if [ ! -e ${target} ]; then
+            echo -n "Migrating ${source} to ${target}... "
+            if [ -d ${source} ]; then
+                # Since most of this will be many small text files,
+                # compress and stream the data (instead of mv) in case
+                # DATA_EXPORT_DIR is actually mounted over a network.
+                tar cpz -C / ${source} 2> /dev/null | tar xpzf - -C ${DATA_EXPORT_DIR}
+            else
+                [ -d ${target_dir} ] || mkdir -p ${target_dir}
+                cp ${source} ${target}
+            fi
             echo "done."
         fi
-        # Unlink the container directory and symlink the exported one in.
-        echo -n "Symlinking ${DATA_EXPORT_DIR}$dir into place... "
-        rm -rf ${dir}
-        ln -s ${DATA_EXPORT_DIR}/${dir} ${dir}
+        # Unlink the source and symlink the target in.
+        echo -n "Symlinking ${target} into place... "
+        source_tmp=$(mktemp -u ${source}.XXXX)
+        mv -f ${source} ${source_tmp}
+        ln -s ${target} ${source} && rm -rf ${source_tmp}
         echo "done."
     done
 fi
