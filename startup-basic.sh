fab61100387d24fa6633c273e9d6fd0ecae8a20a
Brian Claywell <bclaywel@fhcrc.org>
2014-11-07 21:25:39 -0800

    Rewrite startup script for running as galaxy.
    Also add support for multiple processes, as configured in galaxy.ini.

--- startup-basic.sh
+++ startup-basic.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-# Replace this shell with the new Galaxy process.
-exec su -c "sh run.sh" galaxy
+servers=`sed -n 's/^\[server:\(.*\)\]/\1/  p' config/galaxy.ini | xargs echo`
+for server in $servers; do
+    rm -f $server.pid
+    echo -n "Starting $server... "
+    python ./scripts/paster.py serve config/galaxy.ini --server-name=$server --pid-file=$server.pid --log-file=$server.log &
+    sleep 7
+    echo "done."
+done
+
+tail -f *.log
