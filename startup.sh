90666b529b95447529f085883b487b0194d5180e
Brian Claywell <bclaywel@fhcrc.org>
2015-01-28 13:59:33 -0800

    Add multiple process support to startup script.
    It's up to the user to modify galaxy.ini and job_conf.xml as necessary
    to actually *use* multiple processes. Also note that nginx is configured
    only to proxy for the process named 'main' running on port 8080; as
    such, this change by itself is really only useful for adding job
    handlers.
    
    The server processes *must* be restarted after tool installation in
    order for all the processes to see the new tool. This is a known issue.

--- startup.sh
+++ startup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 
-# Replace this shell with the new Galaxy process.
-exec sh run.sh
+# Start the server process(es).
+servers=`sed -n 's/^\[server:\(.*\)\]/\1/  p' config/galaxy.ini | xargs echo`
+for server in $servers; do
+    rm -f $server.pid
+    echo -n "Starting $server... "
+    python ./scripts/paster.py serve config/galaxy.ini --server-name=$server --pid-file=$server.pid --log-file=$server.log --daemon
+    sleep 5
+    echo "done."
+done
+
+exec tail -f *.log
